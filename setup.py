c1552cc0498974e1296f9cd2c410d6be0e92d850
Zsailer <zachsailer@gmail.com>
2018-07-19 10:38:29 -0700

    update requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0',
+                'altair>=0.2.1']
 
 setup_requirements = ['pytest-runner', ]
 
